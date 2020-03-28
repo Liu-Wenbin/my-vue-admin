ab7251e88c577b772e868d4821980532a7d587b3
Liu-Wenbin <877882513@qq.com>
2020-03-28 18:02:11 +0800

    nothing

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="login">
     <div class="login-box">
+      <!-- logo区 -->
+      <div class="logo-box">
+        <div class="flex-wrapper">
+          <h1 class="name">Vue Admin</h1>
+          <h3 class="disc">后台管理系统</h3>
+        </div>
+      </div>
 
+      <!-- 登录信息区 -->
+      <div class="form-box"></div>
     </div>
   </div>
 </template>
@@ -29,9 +38,34 @@
       position: absolute;
       top: 50%; right: 9vw;
       transform: translate(0, -50%);
+      display: flex;
       width: 600px; height: 370.8px;
       background: #fff;
       border-radius: 4px;
+
+      .logo-box {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: center;
+        width: 229.2px; height: 100%;
+
+        & > .flex-wrapper {
+          .name,
+          .disc {
+            text-align: center;
+          }
+          .disc {
+            font-size: 26px;
+          }
+        }
+      }
+
+      .form-box {
+        flex: 1 1 auto;
+        height: 100%;
+
+      }
     }
   }
 </style>
