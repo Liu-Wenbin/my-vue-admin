5e27cb2a9295eb9c3d3294ce8bafad86e57777a7
Liu-Wenbin <877882513@qq.com>
2020-07-16 10:49:20 +0800

    none

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -151,8 +151,8 @@ export class Tool {
   static scrollToTop(el) {
     c = el.scrollTop || document.documentElement.scrollTop || document.body.scrollTop;
     if (c > 0) {
-        window.requestAnimationFrame(scrollToTop);
-        window.scrollTo(0, c - c / 8);
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 8);
     }
   }
 
